38906d196450b358518a951d50252f7e07e8cace
Chanyf123 <chanyifeng123@gmail.com>
2026-03-13 16:46:23 +0800

    Implement EditCommand class to edit student attributes within the student list.

--- src/main/java/seedu/tutorswift/command/EditCommand.java
+++ src/main/java/seedu/tutorswift/command/EditCommand.java
@@ -1,4 +1,31 @@
 package seedu.tutorswift.command;
 
-public class EditCommand {
+import seedu.tutorswift.Ui;
+import seedu.tutorswift.StudentList;
+import seedu.tutorswift.Student;
+
+/**
+ * Represents a command to edit student attributes within the student list.
+ */
+public class EditCommand extends Command {
+    private final int studentIndex;
+    private final String newName;
+    private final String newLevel;
+    private final String newSubject;
+
+    public EditCommand(int studentIndex, String name, String level, String subject) {
+        this.studentIndex= studentIndex;
+        this.newName = name;
+        this.newLevel = level;
+        this.newSubject = subject;
+    }
+
+    @Override
+    public void execute(StudentList students, Ui ui) {
+        Student studentToEdit = students.getStudent(studentIndex - 1); // Subtract 1 to use 0-based indexing
+
+        studentToEdit.editStudent(newName, newLevel, newSubject);
+
+        ui.showEditSuccess(studentToEdit);
+    }
 }
